373ddfdf203f4eb1e853b8d9c53c7f5a5952aa9c
Oihan-B <oihan.barreneche@etu.estia.fr>
2025-06-20 17:11:20 +0200

    IMAGE OMMMGGG
    J'ai enfin réussi à mettre l'image dans le site web Samuel a le seum parce qu'il a un putain de TP LINK de 170

--- main_esp/main_esp.ino
+++ main_esp/main_esp.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <LittleFS.h>
 
 #define RXD2 16
 #define TXD2 17
@@ -175,7 +176,7 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
     }
     .state .row .cell { flex: 1; text-align: center; }
     .state img {
-      flex: 1; max-width: 100%; height: auto; object-fit: contain;
+      flex: 1; max-width: 40%; height: auto; object-fit: contain;
       margin: 30px 0;
     }
     .state .grid-info {
@@ -187,7 +188,7 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
     /* ===== 3. Journal & Carte ===== */
     .log { justify-content: flex-start; }
     .log h2 {
-      margin-bottom: 40px; font-size: 24px; text-align: center;
+      margin-bottom: 30px; font-size: 24px; text-align: center;
     }
     .log-box {
       flex: 1;
@@ -203,6 +204,7 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
       white-space: pre;    /* <=== pas de retour à la ligne automatique */
       word-wrap: normal;   /* pour être sûr */
       word-break: normal;  /* pour être sûr */
+      margin-bottom: 20px;
     }
     canvas {
       border: 1px solid #999; background: #fff; flex: 1;
@@ -243,7 +245,61 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
         <div class="cell">Statut : <strong><span id="robot-state">–</span></strong></div>
         <div class="cell">Batterie : <strong><span id="battery">–</span>%</strong></div>
       </div>
-      <img src="test.png" alt="Robot BILLY">
+      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAc4AAALQCAMAAAAuDGD0AAAAAXNSR0IArs4c6QAAAAlwSFlzAAAOwwAADsMBx2+oZAAAAFdQTFRFR3BMLC
+      kiFRIRLislJyQiKSYkMS4qHhwaFhUSBAMDAQEBAAAACQkJ69W56tS42cWrwrCZ+p5ino98kIJxhHhoZVtQU1JSRUA4Li4uJCEeCgBiBwBJAAAAtDO33gAAAA10Uk5TAA8ZME1tiaK
+      6x9nl+dsgVBUAABWRSURBVHja7Jxpbus6DIXNQaQocgMP8P7X+W7i8fYWmQfD5of+aYq2jI4oMeKRuyRJkiRJkiRJkiRJkiRJkiRJkiRJkiRJkiRJkiRJkiRJkiRJkiTZB4BISCcQE
+      brtA4BIc8CQCk4AEnMR1aq1Vq0qUpgIYdMRF5E5YJXCTAipJDFLbR7R9+ev6E+EN1PZ4gidp55a8+jHiIeQ3VvVcmRNAeeB+cmiaSGELU0+rZcDrsJ0REWRi3r0V4hoWgg3ISYVtbg
+      esVdh7A4FENe2Fi3cvZlV+0Nz91iNWzTlr6cosrT4EXEzs1rN2s+I3QrBgRJTzBexrA6lxFAk0gkuotVarGY8wXcTcxVMOwVcmIiQxnqcuch6HQ47SooCybLIei1Mv9b5AMilzoPoTa
+      j7Elgs5jhMzgHDv/ECEmvzfsSV8QhiThKFXd0WcZUWYULfEbNOEbQqjHClvJPqMQsK3a6hYjEOzW1FKyDNOepW8ONisvqclzdV2Yis017ShPYsKNe4f2uBuQwJV+4+CskkphaEO/ba6u
+      MbLdjtFBJ/dNmcl+j2yeFBtnhwH8R5GdJ9JiiMqRkPVfE4CPrRDYnUZzEf21f2m6AobRibR2crsPo4PPDh6YdPvmWhbmeQjussw9Nrnwt+QM3iz2tBGv2JyrDHGujJD49A6mOGf2gxiS
+      eFwGIxrSj7gVt/wp6epFimUe7ex5JW8fw6QMNE9v1soDBqoPS6PLe36snan7ACr8tzwb2o6eOO98p6szG8uwiq/KI/Z4OetAs1eRj+l+0eeJ4eYdS9CazzYvIqPeOsJ+xBTh2S6eXpzt
+      2boP9Oi8mrBn/ZQCvuQU6J27Y6ADwBcJueTm+T04at7oUBn/VU7HYAqildPZblIqInRG6w3HA1wTfunbXAVZ/JOmAmvLbjW+W9mILgSs9Eh8bvZKQKv2oSgrf6ExAvJiWxzOavmFrawnQ
+      5YDyGM2HpUa/xtk3PDZLor+6vaKaFoDswWOoFM1Vsr6GPrH4h4DA5rqAkFstIeDOrtVpzj+XlWnBLYlZfB9ys/hNxU4ZDilksFveXTFaqwfi1+L58M4IuxoQYPdKEOERcyuL7iibHExS4
+      xspMBfBLteGz5WYbPaFZL53tauuAi7Z+sSAcChK/6qFF1sWCsIXpd81DizRNQVc6YGpGu1i8wsqCQBtpvlcmuGgSGgQNYzyOmjYbF2+1IFSG7y60txZmVKyfOg6HAIrfYUyAyYLwRT2p9
+      idC6B4LglB3BMTvMm0A6dSS+drWcJ9DBItNLZn9Q3e3K1GGX6Fv5ea94gDZ1LHeO1AGM8EDdobyJTkf2ApJY5iAu4ejj8oPFE/OX5Kz9X0TvN9sFIqHOKd9oEhgVe6+BGtleOxtHgHsHg
+      Cg+yjP/+98mkmyG5CYS5kb+9sH8BRwPoLmF4ilNvfwcG9atq8oEKs193PAJpyKzgCJzY6EyVuyaUGR/3YkRDPhFHS5x/ETr9sdHvjNkhBNKLVcNT4j3JtHRL/tJhOJrwwJS8RhmaBQ2vw
+      YicI8PtBlw9fogDXGgLVMEdtyefDQgMT0TAgEmC2s2oZXN6gnz5fA54AB55vWit2R4R/dFZhe1+nUfWNg/bv9CvDjnhh0B0ZiufaJNH6Mg/kip3O3Magt0w+IeQh46ctWOHh2RqXxjNM9
+      InxyNnLbZHZa33uB8Z6Cxx/cBIfv4+jZiWJKq0vK64qCtDJssBCvvGrCrmtaKPXwtRDC2ABbmBMWtxjwEBWq9yusnH+WZ0PrZye42VjyV+o2DehQ3zYbz7PyM+cC2XisgoCjO1lx8+61i
+      MqIOB5qWZ4JTZDWqkLwP3tnot0mr0VhNDFJERRjA/fX+z/ndYzd05hDQK0HHO+90mbVcbpSvm6dQQdx6YfmeZ5uHGd2lJGXlvPnz6/B8SLxqa9/wg8MQRAEQRAEPVlSKanWSUolv5OS8v
+      Nd5487SV59LLzz259ZSf5vuPwzpOLeI5PtypT2qPL0q1yQHX/Z+S+P79iI7O8fyn7znmXZyyc7qtSb3vKC4uSAEzgfhrOpoPVym8f5Aa2XC04lm5UBzniccOcPwmnhTuCEO4ET7oQ7wQj
+      uBM5nSkhWyljgjMWpJSOlxANppiUvG0LoAGmthnBUySt9HE+ZhRkBZxzOeWXysQ+7cayAM0LD/FUMqXyoO/sDK+CMw9kdOA0hPHKxTYHzB+EUwHlfnD3cCXfCncAJd8KdENyJVAjuhDvh
+      TsRO4IQ7odU4DdyJVAjuxGILdyJ2QoidiJ1wJxZbuBOxEzgROyG4E6kQ3Al3wp2IncAJd0KoO4ET7kTshDvRswVOxE7gROy8maq6aXdNhdj5I3DWQ/iU2yF2vj5O34aL9h6x89Vx1oHUI
+      na+Nk7vhxBINWLna7uzCX9qD3e+Ns59CCTXYb/zB+EM3fYWW7jzH3Ais31tnDvEzp/kzir8qWZjsROZbazaQOo83Lk9nFVV11XF1JhV3dSVv3qxCxcN1QZ7tsmbu9PvhxOb9gqOb/rT64
+      f668vVPoyiL2zJncl7u7Pqw1ndnzw99WaHa2z1YRiGrjnaFu7cGE7f86GQaIbgJjb0VbXJaYS3d2fLJ6q+coF04I0Id24HJ5mTNMxgDjM4ETu3hdN7F9gdEj98rS/hztdwZ+BxfoQvauH
+      O13BnYFzI4IQ7t+1OflGlfLXnXUtCZis3iLMLgdR5z25TD8hsN+5ODpurGdsyGye+bpqmrhA7t4KTLzxbT8R8Hci0X8nV/WjZBu7cHk7furM3249PnMTz4s/DF5q+cYQf7twYTu99dTgt
+      qJW/en0E3dd+djCz2VoqBHd67z98XXvu9Y+qrsiyTCpcw50vtH3tlwYzW8TOzeKMb9mHDu58aZz+8BUn3InBTHSFNrrY7uFODGYidm5zte093PmKOPn5haHCjsrL46wOF2/W6Nm+Lk5S3
+      Q1u6Hf+Az3bV8ZJ3b+qqjz2O18dJ/H0HrNCOPULOyoQ3HlzYVYI7oQ7oRe6XTcLIThWwBmH07EK98IptBbTF7MwJ+CMw8nrXjhVZm0mJzhNcVJ+VpEXoyxwRuMsi7PGC3m5lub2OIXKQw
+      ilYpZbKYWQQsrLbyfJ1IUGlCLkgtVSjJJSivGqfkrcw5uBcK6RAc5onOpxGY8LIbhUAOcPwCnSWJoJFtvt4jT2QhPufH2cuvikmckEOH8ATpmHo/IomgI4N4pTpuGoQiVHIXa+PE5tQwh
+      WJ0mCxfauON0jcKri3GiCO38ATpE6Cpwbwunb8GB1G8IplRLJ30gPIYRSJxvBSXpnnDq3ZSr/dgMspGJrOP1buzN1Ibi/4WnsXFartJZPXmz/e6BC6LeFM1gTYzLaznTM9wldOJuK57rz
+      v/89TptxJ6Wn8Ty1CyFkYqZRVMjnufNdcRLP+JRG5TMlpzo3iuDO5+AUI89cRXYQeGgyOy/ecOez6k5dBkqHYua6NFuLjjThzofjJKvFhk9d8ubU435ZkiRw59NwJsZS+Fxf3pi5tt8RM
+      9z5JJwU8XIVtTFWSobUWIvCnU/DSbZyacTqzL1bled0F+58Ek4CFDGOJ1L2zTI7D5rAnc/ESSlpJld6mU2EjBsbCHDnU3ESImfE6o5QKlhzmiSBOx+Jk++QC0PZ0Kq8Vq/qLAiplIA774
+      dTFc6mcqYzYM2qPJjNa9nOgsptaQTceTecem5HTDOhj5UsuO67sqdvZ5Zwq+DOu+E0dqbUF8uxj+oRp7lEyKVcPVtKuPMuOGmKwJqFzt1CD8kqdgVWXAdXI3beD6dQOU3Hcn31VZlQKbk
+      WQia5XjAy2/vgpCvP15h6Ve0pTvvT7PcaMTNNJLWEO+9Vd+qSn9pSxZrWkDz5kGsUBTUx55gcybRUcOc9cFL/ptScPZeTIcU1bGU+SWFFdgmcKgtWwZ13wUlRMpuuo6dkSCzgdBQQ54oX
+      iqbn/BbuvBdOqgen9swceWxOevxWpngxk/c5fc5vS7jzTjip+swl26hLF76VC7BqLF64ykWNtSfceUecMmeLEkWlZxxObQMho7+qkOckKRVw581wKq3kqqJEjJaK3OzkcdoTxTGqFgp15
+      81wqtxNurSSBmWZgBeBkxbvQkwL0bNLU4Gu0M1w6nG541rxbBVixBLOYoLTsTj16Fun0RW6oTvH4VeuuSMWBg2mkn+DU6FnezucIuPuW0ipKJnM6MGd28RJkPgGeSoYWFbF4KTYKWdjp4
+      E7b4NTKjGuoZMUR1HKeUXB6n/ObOl/y1j7ILO9Dc40N+LIwIz2nHb6FJsLfYfTRBQqubxMhKHuvAlO45wR7O6yYIsSUS71hYyb4jx50KppFFaXoRV0hW6EM5SGH1xXjjNizmx/LS/HFCH
+      Jxaf3nbiiZ3s7nGNOK4pJyirHAp8ZyMsXcC7tqNCynY1fw47KrXDa0W3jcOx0OcwkMzpSxO93Tne+yZW6eII7H/TxYJzyMhqp7WQ5PBmRxSmSeUnL7JUKJq8yv9vCqij1Dz4mqn8MTppu
+      N4J6p1clh+QyHRE7KyRoVoi7T0JpiXOFboEzUcO5YCgmSyuP09KLvGgIfrKBIudmWMQj687hwTo8Emc+1gkyH7HOlxyE036L08x2BwvFjPTmCkcs3ggnZSSnzy4CZ+QUvDBMm/98KIoEz
+      tu5szu5c3RpdhOcBUXh6y6QYEZASwWcN46dPM742Enr6uQt3H64MDa4DO68cWabRma2cnEeITD3dzJlrFCploidN8Op8u/rziK6UKG7r1eNqwicNn3zrpAYMTk9uf7RbQTaLOGSIXqZtI
+      zT++oo/77E6Cp4v9SzHS0zjhlcpzQuE8zkdJ4wYub9uFlsE4+z2veDc0PXVu9Ms24PXdcd9jWLky6eS+kWg1GUJDkjYndU+HtZ6B4jFYfT+8aFs4bav60zm77bt7t23/X76jucLpV0ZwE
+      zhMm08NLl2+nZFTkbu7Rx7mwDyb1rVK32Xftr1K4/VLM4TWnEyU+TuCZTLnWRljMef6wF12Agw6/D6Xfhi+r3NGfb7X791qGr5nBq9Tu/dUyHvIycFZovSqgqsjoCZ+3CF/VvGT+b/kiT
+      NOwmOJlHnigGiWDuQLFq1RmLg5o5eKHUYjXOfbhS846B87D/9afarmJxEk1+ztbppTODeNEoGX+0vF6Nc8BTzo+Rc1xqSV3j559BprhDg2UeMQXPR8lSzx1dnYt1OH0dJvLv5866//VV+
+      /03ODVnJe24G7DVsPCsG/4YIZLQNuZJDU0gvW8ytOuucR48t9iSlXK1qiVn1pxFww8HUdfduUwA53r5JgpnolMtuSeIFXLxsCdeNAnE+TNN1drYicX2U3U/wbn67mviFkzM3ddclOWNLM
+      TaQsX7cK3hHXu1fXOVCu3icPIRjrZdliX0moPf4wuV3cf76bpQ2fV1FE45thVWnlzCS2SOWvixOGdT2+Et2wi7r/bs24/VOOksYsHmN3NFRkRLL6bJt3/nLgJdha4hmnumyRf70GraKbn
+      b41f4FvwhBFL7njQ/qgPx7Lr6IwanGu/eFezrpYp8Imsm/w1nRVsqbve2G2RV23dt86tp992hjjsxM6ckhjucJvrpZeKfcHpfd0MIwQ2H6uNtcXpfH7q+77sD9fdYnLynrF44bjr+6WXx
+      OClTr486wvRvPFkyXoXKf0ThlJmjDi5bdEZIjs19IyJxMv8WTArRpwic2lLAYwoPk0RJLe+IYZIvTnE4DW198o9aiJMqYnka4LwhTlU6x9EUGa3B8TwLBXc+BadQqVHsIkyujZE4pbcWO
+      J+Ak2+Q00lSRiTx0p9+B8474rTRLjNUi0ZLf/odsXNDODUzLBIlZLYbwqnyMQ8CzpfHSaN+Cjh/BE5DOyx3lwDOOz9dV5X3X2qVMUZrbYzJXDi00Hq5ENLT1TNH/b+9O2pNEArAMOxxWi
+      sPbRjDhP7/71yzC28aGmoe4/lksIvuXp7Mwgoj3wjY5wubjLGK1W3xas8uxip2f8ePcW+7HpY9ceY7TeZYVQzWjP3DFs3Z/Ni0tX2noV9nXT7n2auaaTs1g69xy/unn0HOt8i5738Je8k
+      FOV+Ss6hi9z1OdKZfc0TOUOzKW006t6+z/x4nOrevsx+dm5icdMpJp5x0Gp10ykmnnHTKSafRSaecdMpJp5x0Gp10ykmnnHTKSafRSaecdMpJp5x0Gp10ykmnnHTKSafRSaecdMpJp9Fp
+      dNIpJ51y0ml00iknnXLSKSedRiedctIpJ51y0ml00iknnXLS+fKc39cE1tIpJ52Pc8Z1R+e8OS+rrqVTTjrplJNOOemUk0456aRTTjrlpFNOOuWkk0456ZSTTjnplJNOOuWkU0465aRTT
+      jrplJPOJHPGdtWDTjc10OkelffP+VXX59WPmk6j093XctIpJ51y0ml00iknnXLSKSedRiedctIpJ51y0ml00iknnXLSKSedRiedctIpJ51Gp9FJp5x0ykmn0UmnnHTKSaecdBqddMpJp5
+      x0ykmn0UmnnHTKSaecdBqddMpJp5x0ykmn0UmnnHTKSaecdBqddMpJp5x0CkKn0UmnnHTKSafRaXTSKSedctJpdNIpJ51y0iknnUYnnXLSKSedctJpdNIpJ51y0iknnUYnnXLSKSedctJ
+      pdNIpJ51y0iknnUYnnXLSKSedRqfRSaecdMpJp9Gpp5x0ykmnnHQanXTKSaecdMpJp9FJp5x0ykmnnHQanXTKSaecdMpJp9FJp5x0ykmnnHQanXTKSaecdMpJp9FJp5x0ykmn0Wl00ikn
+      nXLSaXTSKSedctIpJ51GJ51y0iknnXLSaXTSKSedctIpJ51GJ51y0iknnXLSaXTSKSedctIpJ51GJ51y0iknnUan0UmnnHTKSafRqaacdMpJp5x0Gp10ykmnnHTKSafRSaecdMpJp5x0G
+      p10ykmnnHTKSafRSaecdMpJp5x0Gp10ykmnnHTKSafRSaecdMpJp9FpdNIpJ51y0ml00iknnXLSKSedRiedctIpJ51y0ml00iknnXLSKSedthGduZx0mnMnnXLSKSedRqfRSafrTjrpdO
+      40OvWUk0456ZSTTnPdSSeddMpJp5x0Gp10ykmnnHS67qTTnDvplJNOOemUk06jk0456ZSTTjnpNDrplJNOOemUk06jk0456ZSTTqPT6KRTTjrlpNPopFNOOuWkU046jU465aRTTjrlpNP
+      opFNOOuWkU046jU465aRTTjrlpNPopFNOOuWkU046jU465aRTTjqNTqOTTjnplJNOo1NNOemUk0456TQ66ZSTTjnplJNOo5PONZfLOX1y0unJlk466TQ65Uwl58kmLa2cl8amLSaU02ZY
+      VWQpLJQHm77jZxo6s5B3C/ej+8/+XXh4/C0zMzMzMzMzMzMzMzMzsyf2C9zGtVFvFjRMAAAAAElFTkSuQmCC" alt="Robot BILLY">
       <div class="grid-info">
         <div class="col">
           <div class="line">Vitesse Moteur G: <span id="motor-left">–</span> m/s</div>
@@ -435,6 +491,11 @@ void setup(){
   Serial.begin(115200);
   Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
   delay(500);
+
+  if (!LittleFS.begin()) {
+    Serial.println("Erreur lors du montage de LittleFS");
+    return;
+  }
 
   Serial.println("\n=== Démarrage BILLY ESP32 ===");
   WiFi.softAP(ssid, password);
